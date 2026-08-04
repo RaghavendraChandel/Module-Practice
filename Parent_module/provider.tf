38900e0913ca76083ac941bdaca93d5d3fc4f66a
RaghavendraChandel <chandelraghavendra13.com>
2026-08-04 23:01:10 +0530

    backend implement

--- Parent_module/provider.tf
+++ Parent_module/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "4.80.0"
     }
   }
+  backend "azurerm" {
+      
+    resource_group_name  = "rg-prod-webapp-centralindia-01"          
+    storage_account_name = "stprodapp002"                              
+    container_name       = "tfstate"                            
+    key                  = "prod.terraform.tfstate"                
+}
+
 }
 
 provider "azurerm" {
